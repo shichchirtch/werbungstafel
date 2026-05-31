a2726907394520328cc710793c312dde237715bf
shichchirtch <ivanvol9l@gmail.com>
2026-05-31 02:39:20 +0200

    17 Commit
    import ForeignKey

--- back/postgres_table.py
+++ back/postgres_table.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, BigInteger, String, ARRAY
+from sqlalchemy import Integer, BigInteger, String, ARRAY, ForeignKey
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from config import settings
