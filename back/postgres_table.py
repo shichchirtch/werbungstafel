319866bc9ee87c0fd5046d9a19d494dcddea2fc7
shichchirtch <ivanvol9l@gmail.com>
2026-07-02 22:37:48 +0200

    98 commit
    debug

--- back/postgres_table.py
+++ back/postgres_table.py
@@ -85,6 +85,6 @@ class Message(Base):
 
 async def init_models():
     async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
