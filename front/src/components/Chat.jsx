4bce0970f8b2d4d63c1899d6f1f95862d00c367d
shichchirtch <ivanvol9l@gmail.com>
2026-07-28 03:19:40 +0200

    210 commit
    Мимальная высота окна чата ограничена 220 px

--- front/src/components/Chat.jsx
+++ front/src/components/Chat.jsx
@@ -302,6 +302,7 @@ function Chat({adId, senderId, receiverId,}) {
         overflow-y-auto
         custom-scrollbar
         max-h-[70vh]
+        min-h-[220px]
         resize-y
         flex
         flex-col
